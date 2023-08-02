8637027ba15b508af2dd8e3ecec6bb91b8b958b5
Ycow <skypnal12@gmail.com>
2023-08-02 17:15:23 +0900

    feat: 게시글 생성 dto 추가 및 적용

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+}
