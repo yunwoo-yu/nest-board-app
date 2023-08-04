46588a4ed7512c1e4558dcb4231154a19562d08e
Ycow <skypnal12@gmail.com>
2023-08-04 22:32:07 +0900

    feat: 커스텀 유효성 검사 class 생성

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(value: any) {
+    const result = this.StatusOptions.includes(value);
+
+    return result;
+  }
+}
