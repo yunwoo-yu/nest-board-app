c7870c8e524977d143e51d54f3a97effdb7e66f4
Ycow <skypnal12@gmail.com>
2023-08-16 21:44:19 +0900

    feat: bcrypt 비밀번호 salt + hash 암호화 작업

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,13 +2,17 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth.credential.dto';
 import { ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
 
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
