0e1723734716ea200ca4de802759682180993a62
Ycow <skypnal12@gmail.com>
2023-08-20 23:54:05 +0900

    fix: 변수 네임 컨벤션 변경

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,16 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { UserRepository } from './user.repository';
-import { AuthCredentialsDto } from './dto/auth.credential.dto';
-import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcryptjs';
+import { AuthCredentialsDto } from './dto/auth.credential.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
-    private JwtService: JwtService,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
@@ -26,7 +26,7 @@ export class AuthService {
     if (user && (await bcrypt.compare(password, user.password))) {
       // 유저 토큰 생성 (Secret + Payload 필요)
       const payload = { username };
-      const accessToken = await this.JwtService.sign(payload);
+      const accessToken = await this.jwtService.sign(payload);
 
       return { accessToken };
     } else {
