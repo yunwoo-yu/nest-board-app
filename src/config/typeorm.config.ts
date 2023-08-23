5463869ec2ecc55b90e591cd2a0146cfc0e27b26
Ycow <skypnal12@gmail.com>
2023-08-24 01:19:36 +0900

    feat: yml 파일 & 환경변수 고려해서 적용

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -8,8 +8,8 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: process.env.RDS_PORT || dbConfig.port,
   username: process.env.RDS_USERNAME || dbConfig.username,
-  password: 'postgres',
-  database: 'board-app',
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
+  synchronize: dbConfig.synchronize,
 };
